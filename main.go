5ddbb992d7157ef2c342ea5245dca2bd87302557
Vineeth Kumar Milatari <vineethkumarm8@gmail.com>
2025-11-13 20:11:31 -0500

    fix: remove duplicate blank lines and trailing whitespace

--- main.go
+++ main.go
@@ -8,25 +8,24 @@ import (
 func homePage(w http.ResponseWriter, r *http.Request) {
 	// Render the home html page from static folder
 	http.ServeFile(w, r, "static/home.html")
-	}
+}
 
 func coursePage(w http.ResponseWriter, r *http.Request) {
 	// Render the course html page
 	http.ServeFile(w, r, "static/courses.html")
-	}
+}
 
 func aboutPage(w http.ResponseWriter, r *http.Request) {
 	// Render the about html page
 	http.ServeFile(w, r, "static/about.html")
-	}
+}
 
 func contactPage(w http.ResponseWriter, r *http.Request) {
 	// Render the contact html page
 	http.ServeFile(w, r, "static/contact.html")
-	}
+}
 
 func main() {
-
 	http.HandleFunc("/home", homePage)
 	http.HandleFunc("/courses", coursePage)
 	http.HandleFunc("/about", aboutPage)
@@ -34,6 +33,6 @@ func main() {
 
 	err := http.ListenAndServe("0.0.0.0:8080", nil)
 	if err != nil {
-	log.Fatal(err)
+		log.Fatal(err)
 	}
 }
